24a3f2c96c58db7347ca94f74d02eb4a4de463b2
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-04-16 13:03:37 +0200

    async load Fixed #39

--- shinyapp/R/country/country_Server.R
+++ shinyapp/R/country/country_Server.R
@@ -6,6 +6,8 @@
 
 # -- Library
 library(geojsonio)
+library(promises)
+library(future)
 
 
 # -------------------------------------
@@ -25,6 +27,18 @@ country_Server <- function(id, r, path) {
     # -- ids
     group_id <- "countries"
     
+    
+    # -------------------------------------
+    # Init
+    # -------------------------------------
+    
+    # -- Panel (waiting for file to be loaded)
+    output$panel_ui <- renderUI(
+      wellPanel(
+        h4("Countries"),
+        p("Loading country boundaries in progres...")))
+    
+    
     # -------------------------------------
     # Load resources: iso countries
     # -------------------------------------
@@ -38,7 +52,6 @@ country_Server <- function(id, r, path) {
                         "latitude" = "numeric",
                         "longitude" = "numeric")
     
-    
     # -- load country ISO list
     countries_iso <- kfiles::read_data(file = filename_iso, 
                                        path = path$resources,
@@ -49,35 +62,83 @@ country_Server <- function(id, r, path) {
     r$countries_iso <- countries_iso
     
     
-        
-    # -- load data
-    cat("[countries] Read countries geojson data... \n")
-    countries_geojson <- geojson_read(file.path(path$resources, filename_geojson), what = "sp")
-
+    # -------------------------------------
+    # Load resources: geojson data
+    # -------------------------------------
+    
+    # -- set async strategy
+    plan(multisession)
+    
+    # -- init
+    geojson_data <- reactiveVal()
+    
+    # -- async read data
+    cat("[countries] Asynchronous -- start reading countries geojson data... \n")
+    future(
+      geojson_read(file.path(path$resources, filename_geojson), what = "sp")
+    ) %...>%
+      geojson_data() %...!%  # -- Assign to geojson_data
+      (function(e) {
+        geojson_data(NULL)
+        warning(e)
+        session$close()
+      }) # -- if error
+      
+    
+    # -- observe when geojson_data is ready
+    observeEvent(geojson_data(), {
+      
+      cat("[countries] Asynchronous -- read countries geojson data done. \n")
+      
+      # -- notify user
+      showNotification("Country boundaries are now available", type = c("message"))
+      
+      # -- update ui
+      output$panel_ui <- renderUI({
+      
+        wellPanel(
+          
+          h4("Countries"),
+          p("Displays visited countries.", br(), "(locations marked as *been there*)"),
+          
+          # hide / show checkbox
+          checkboxInput(ns("hide_show"), label = "Hide / Show", value = FALSE))})
+      
+    })
+    
     
     # -------------------------------------
     # Event observers
     # -------------------------------------
     
     observeEvent({
+      geojson_data()
       r$visited_countries()
       r$filter_country}, {
-        
+
+        # -- because ignoreNULL = FALSE
+        # need to wait for async data to be ready
+        req(geojson_data())
+
+        # -- get visited contries
         selected_countries <- r$visited_countries()
-        
+
+        # -- apply filter
         if(!is.null(r$filter_country))
           selected_countries <- selected_countries[selected_countries %in% r$filter_country]
-        
-        data_map <- countries_geojson[countries_geojson@data$ADMIN %in% selected_countries, ]
-        
+
+        # -- selected geojson to be displayed
+        selected_geojson <- geojson_data()[geojson_data()@data$ADMIN %in% selected_countries, ]
+
+        # -- update map
         r$proxymap %>%
-          
+
           # -- cleanup
           clearGroup("countries") %>%
-          
+
           # -- add areas
-          addPolygons(data = data_map, weight = 1, color = "red", group = "countries")
-        
+          addPolygons(data = selected_geojson, weight = 1, color = "red", group = "countries")
+
       }, ignoreNULL = FALSE)
     
     
--- shinyapp/R/country/country_UI.R
+++ shinyapp/R/country/country_UI.R
@@ -10,15 +10,7 @@ country_UI <- function(id){
   # namespace
   ns <- NS(id)
   
-  # UI
-  wellPanel(
-    
-    h4("Countries"),
-    p("Displays visited countries.", br(), "(locations marked as *been there*)"),
-    
-    # hide / show checkbox
-    checkboxInput(ns("hide_show"), label = "Hide / Show", value = FALSE)
-    
-  )
+  # -- ui
+  uiOutput(ns("panel_ui"))
   
 }
