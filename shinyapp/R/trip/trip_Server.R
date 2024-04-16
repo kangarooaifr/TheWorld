f23d206d14fb4057a116edb066ccb7262bf3a1a3
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-04-16 16:31:10 +0200

    Fixed #61

--- shinyapp/R/trip/trip_Server.R
+++ shinyapp/R/trip/trip_Server.R
@@ -10,21 +10,24 @@ trip_Server <- function(id, r, path) {
     # -- get namespace
     ns <- session$ns
     
-    # -- id
-    kitems_id <- "trip"
     
-    # id, trip.id, name, status, description, comment
-    # date.start, date.end, cities, countries >> computed from other tables based on trip.id
+    # -------------------------------------
+    # Transport management
+    # -------------------------------------
+    
+    # -- id
+    trip_kitems_id <- "trip"
+    
+    # id, name, status, description, comment
     # status = draft, planned, inwork, done
+    # date.start, date.end, cities, countries >> computed from other tables based on trip.id
     
     # -- launch kitems sub module
-    kitems::kitemsManager_Server(id = kitems_id, r, path$data)
+    kitems::kitemsManager_Server(id = trip_kitems_id, r, path$data)
     
     # -- items name
-    r_items <- kitems::items_name(id = kitems_id)
-    r_data_model <- kitems::dm_name(id = kitems_id)
-    r_trigger_add <- kitems::trigger_add_name(id = kitems_id)
-    r_trigger_delete <- kitems::trigger_delete_name(id = kitems_id)
+    # r_trigger_add <- kitems::trigger_add_name(id = trip_kitems_id)
+    # r_trigger_delete <- kitems::trigger_delete_name(id = trip_kitems_id)
     
     
     # -------------------------------------
