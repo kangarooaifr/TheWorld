a6b3674a843e6ecd85dba32edda2de47beacebf2
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-04-29 09:06:42 +0200

    update trip ui

--- shinyapp/R/trip/trip_UI.R
+++ shinyapp/R/trip/trip_UI.R
@@ -14,20 +14,16 @@ trip_panel_UI <- function(id)
   # -- return
   wellPanel(
     
-    # -- title
-    h4("Trips"),
-    
-    # -- create btn
-    kitems::create_BTN("tripmngr-trip"),
-    
-    # -- trip selector
-    selectizeInput(inputId = ns("trip_selector"), label = "Select trip", choices = NULL),
+    # -- trip selector, create btn
+    div(style="display: inline-block;width: 200px;", selectizeInput(inputId = ns("trip_selector"), label = "Select trip", choices = NULL)),
+    div(style="display: inline-block;width: 50px;",HTML("<br>")),
+    div(style="display: inline-block;", kitems::create_BTN("tripmngr-trip")),
     
     # -- display options
-    radioButtons(inputId = ns("display_options"), label = "", choiceNames = list(icon("compass-drafting"), icon("circle-check"), icon("layer-group")), choiceValues = list("plan", "done", "all"), inline = TRUE),
+    #radioButtons(inputId = ns("display_options"), label = "", choiceNames = list(icon("compass-drafting"), icon("circle-check"), icon("layer-group")), choiceValues = list("plan", "done", "all"), inline = TRUE),
     
     # -- hide / show
-    checkboxInput(inputId = ns("hide_show"), label = "Hide / Show", value = TRUE),
+    #checkboxInput(inputId = ns("hide_show"), label = "Hide / Show", value = TRUE),
     
     # -- line break
     hr(),
