bf42d3549b0b7cdec4688293e0f7ae28fc18c0cd
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-05-05 11:21:25 +0200

    Add is_road attribute #102

--- shinyapp/R/location/import_stations.R
+++ shinyapp/R/location/import_stations.R
@@ -1,39 +1,62 @@
 
 
 # ------------------------------------------------------------------------------
-# Import UIC Railway Stations
+# Import Stations (Bus & Rail)
 # ------------------------------------------------------------------------------
 # 
 # This is a helper function to import data
 # It should not be used directly by the app
 # 
 
-import_railway_stations <- function(){
+import_stations <- function(){
   
   # -- inside function (not run upon source code)
   library(readr)
   
   # -- param
-  filename <- "shinyapp/resources/railway_stations.csv"
+  filename <- "shinyapp/resources/stations.csv"
   
   # -- columns
   cols_keep <- c("id", "name", "slug", "uic", "latitude", "longitude", "parent_station_id", "country", "time_zone", "is_airport", "iata_airport_code")
   cols_rename <- c("internal_id", "name", "slug", "uic", "lat", "lng", "parent_id", "country", "time_zone", "is_airport", "iata")
   
   # -- read file
-  railway_stations <- read_delim(filename, 
+  stations <- read_delim(filename, 
                                  delim = ";",
                                  escape_double = FALSE, 
                                  trim_ws = TRUE)
   
   # -- slice columns
-  railway_stations <- railway_stations[cols_keep]
-  colnames(railway_stations) <- cols_rename
+  stations <- stations[cols_keep]
+  colnames(stations) <- cols_rename
   
   # -- generate ids
-  railway_stations$id <- ktools::seq_timestamp(n = dim(railway_stations)[1])
+  # Warning! Performance pb ~13min
+  stations$id <- ktools::seq_timestamp(n = dim(stations)[1])
   
   # -- return
-  railway_stations
+  stations
   
 }
+
+
+bus_stations <- function(stations){
+  
+  # -- list of identified keywords
+  keywords <- c("autobus", "autobuses", "autobusni", "avtobusna", "autobusowy",
+                "busstation", "bussterminal", "busterminal",
+                "busbf", "fernbus", "busbahnhof",
+                "routiere")
+  
+  # -- get indexes
+  idx <- grep(paste(keywords, collapse = "|"), railway_stations$slug)
+  
+  # -- init & tag bus stations
+  stations$is_road <- NA
+  stations[idx, ]$is_road <- TRUE
+  
+  # -- return
+  stations
+
+}
+
