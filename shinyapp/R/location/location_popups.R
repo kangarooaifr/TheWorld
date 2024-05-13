cf68389765b62c242d42bfc22d5f92e82cecb068
thekangaroofactory <tkf_greenmonkey@thekangaroofactory.com>
2024-05-13 12:23:01 +0200

    popups

--- shinyapp/R/location/location_popups.R
+++ shinyapp/R/location/location_popups.R
@@ -4,45 +4,71 @@ location_popups <- function(locations, type, activity, ns){
   
   cat("[location] Build popups, activity =", activity, "/ type =", type, "\n")
   
-  sprintf(
-    paste0(
-      
-      "<b>", locations$name, "</b>",
-      "<p>", "lng = ", round(locations$lng, digits = 3), " / lat = ", round(locations$lat, digits = 3), "</p>",
-      
-      hr(),
-      
-      # -- conditional actions
-      if(activity == "world_map"){
-        
-        paste0(
-          actionLink(inputId = "update_%s", 
-                     label =  "Update", 
-                     onclick = sprintf(
-                       'Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})',
-                       ns("action_update"))),
-          
-          actionLink(inputId = "delete_%s", 
-                     label =  "Delete", 
-                     onclick = sprintf(
-                       'Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})',
-                       ns("action_delete"))),
-          
-          actionLink(inputId = "been-there_%s", 
-                     label =  "Been there", 
-                     onclick = sprintf(
-                       'Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})',
-                       ns("action_beenthere"))))
-        
-      } else {
-        
-        actionLink(inputId = "add_%s",
-                   label =  "Add to trip", 
-                   onclick = sprintf('Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})', ns("add_to_trip")))
-        
-      }),
+  # -- compute header
+  header <- paste0(
     
-    # -- >>>>>>>>>>>>> fix this because not all args are used if 2nd options....
-    locations$id, locations$id, locations$id)
+    "<b>", locations$name, "</b>","<br>",
+    "lng = ", round(locations$lng, digits = 3), " / lat = ", round(locations$lat, digits = 3))
+  
+  
+  # -- compute body
+  body <- paste0(
+    
+    "<p>", locations$type, "<p>")
+  
+  
+  # -- compute footer
+  footer <- if(activity == "world_map"){
+    
+    # -- world_map activity
+    sprintf(
+      paste0(
+        actionLink(inputId = "update_%s", 
+                   label =  "Update", 
+                   onclick = sprintf(
+                     'Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})',
+                     ns("action_update"))),
+        
+        actionLink(inputId = "delete_%s", 
+                   label =  "Delete", 
+                   onclick = sprintf(
+                     'Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})',
+                     ns("action_delete"))),
+        
+        actionLink(inputId = "been-there_%s", 
+                   label =  "Been there", 
+                   onclick = sprintf(
+                     'Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})',
+                     ns("action_beenthere")))),
+      
+      locations$id, locations$id, locations$id)
+    
+  } else {
+    
+    # -- trip_planner activity
+    sprintf(
+      paste0(
+        
+        if(type == 'selected')
+          
+          # -- remove from trip
+          actionLink(inputId = "remove_%s",
+                     label =  "Remove from trip", 
+                     onclick = sprintf('Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})', ns("remove_from_trip")))
+        
+        else
+          
+          # -- add to trip
+          actionLink(inputId = "add_%s",
+                     label =  "Add to trip", 
+                     onclick = sprintf('Shiny.setInputValue(\"%s\", this.id, {priority: \"event\"})', ns("add_to_trip")))),
+      
+      locations$id)
+    
+  }
+  
+  
+  # -- merge all & return
+  paste0(header, body, footer)
   
 }
