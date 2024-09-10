a8b52a64ebe698aebf8a965334fec85973e1002b
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-09-10 16:39:54 +0200

    Implement #104

--- shinyapp/R/map/map_click_observer.R
+++ shinyapp/R/map/map_click_observer.R
@@ -12,12 +12,17 @@ map_click_observer <- function(map, ns, coord_digits){
     lng <- map$click()[['lng']]
     lat <- map$click()[['lat']]
     
+    # -- get address info from coordinates
+    address <- reverse_geocoding(lng, lat)
+    
     # -- display popup
     map$proxy %>% 
       clearPopups() %>%
       addPopups(lng, lat, 
                 paste("Longitude:", round(lng, digits = coord_digits), br(),
                       "Latitude:", round(lat, digits = coord_digits), 
+                      hr(),
+                      "address:", address$display_name,
                       hr(),
                       actionLink(inputId = map$id, 
                                  label =  "add to my locations", 
