14d5d7953a1cbb7465045c000da6457d58c06d42
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-09-10 16:30:15 +0200

    Add traces #103

--- shinyapp/R/map/reverse_geocoding.R
+++ shinyapp/R/map/reverse_geocoding.R
@@ -2,7 +2,9 @@
 # https://nominatim.openstreetmap.org/reverse?format=json&lat=52.5487429714954&lon=-1.81602098644987&zoom=18&addressdetails=1
 
 reverse_geocoding <- function(lng, lat, zoom = 18, addressdetails = 1){
-  
+
+  cat("[reverse_geocoding] Calling nominatim API \n")
+    
   # -- init
   address <- "https://nominatim.openstreetmap.org"
   endpoint <- "reverse"
@@ -19,10 +21,13 @@ reverse_geocoding <- function(lng, lat, zoom = 18, addressdetails = 1){
   # -- build query
   url <- paste(address, endpoint, sep = "/")
   url <- paste(url, params, sep = "?")
+  cat("-- url =", url, "\n")
   
-  str(url)
-  
-  # -- call API & return
+  # -- call API
   result <- tryCatch(jsonlite::fromJSON(url), error = function(c) return(list()))
 
+  # -- return
+  cat("-- output osm_id =", result$osm_id, "\n")
+  result
+  
 }
